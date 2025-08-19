2914d6acd61d755f4f9f21216b300e6264623d5b
Evelyn Plesca <evelyn-iulia.plesca@analog.com>
2025-08-19 15:16:12 +0300

    Initial commit
    Signed-off-by: Evelyn Plesca <evelyn-iulia.plesca@analog.com>

---
+++ main.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+#include <iio.h>
+#include <errno.h>
+#include <cstring>
+#include <vector>
+
+#define URI                     "ip:10.76.84.194"
+#define DEV_NAME                "ad5592r"
+#define SAMPLE_COUNT            200
+#define THRESHOLD               100
+#define SAMPLE_FREQUENCY        100    
+
+#define CHANNEL_PREFIX "voltage"
+#define NUM_CHANNELS 6
+
+typedef struct
+{
+        uint16_t xpos;
+        uint16_t xneg;
+
+        uint16_t ypos;
+        uint16_t yneg;
+
+        uint16_t zpos;
+        uint16_t zneg;
+} current_samples;
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+        iio_channel *channels[6];
+
+        // create context
+        iio_context *ctx = iio_create_context_from_uri(URI);
+        if (!ctx)
+        {
+                cout << "Error creating IIO context " << URI << ": " << strerror(errno) << endl;
+                return -errno;
+        }
+
+        struct iio_device *trigger = iio_context_find_device(ctx, "trigger0");
+        if (!trigger)
+        {
+                std::cerr << "Error finding trigger device: " << strerror(errno) << std::endl;
+                return 1;
+        }
+
+        double current_freq = 0;
+        int res = iio_device_attr_read_double(trigger, "sampling_frequency", &current_freq);
+        if (res) {
+                std::cerr << "Error reading sampling_frequency: " << strerror(errno) << std::endl;
+                return 1;
+        }
+        if (current_freq != SAMPLE_FREQUENCY) {
+                iio_device_attr_write_double(trigger, "sampling_frequency", SAMPLE_FREQUENCY);
+        }
+
+        // find device
+        iio_device *dev = iio_context_find_device(ctx, DEV_NAME);
+        if (!dev)
+        {
+                cout << "Error finding device " << DEV_NAME << ": " << strerror(errno) << endl;
+                return -errno;
+        }
+
+        // find channels
+        for (int i = 0; i < NUM_CHANNELS; i++)
+        {
+                char channel_name[10] = CHANNEL_PREFIX;
+                int name_len = strlen(channel_name);
+                channel_name[name_len] = '0' + i;
+                channel_name[name_len + 1] = '\0';
+
+                channels[i] = iio_device_find_channel(dev, channel_name, false);
+                if (!channels[i])
+                {
+                        cout << "Error finding channel " << channel_name << ": " << strerror(errno) << endl;
+                        return -errno;
+                }
+        }
+
+        /*
+            // read channels
+            for (int i = 0; i < NUM_CHANNELS; i++)
+            {
+                double val;
+                int res = iio_channel_attr_read_double(channels[i], "raw", &val);
+                if (res)
+                {
+                    cout << "Error reading channel " << i << ": " << strerror(errno) << endl;
+                    return -errno;
+                }
+                cout << val << ' ';
+            }
+        */
+
+        // enable channels
+        for (int i = 0; i < NUM_CHANNELS; i++)
+                iio_channel_enable(channels[i]);
+
+        // create buffer
+        iio_buffer *buf = iio_device_create_buffer(dev, SAMPLE_COUNT, false);
+        if (!buf)
+                cout << "Could not create buffer:" << strerror(errno) << endl;
+
+        // refill buffer
+        ssize_t bytes_read = iio_buffer_refill(buf);
+        if (bytes_read < 0)
+                cout << "Could not refill buffer:" << strerror(errno) << endl;
+
+        cout << endl;
+        cout << "All ok!" << endl;
+
+        // Declare vectors to store axis data
+        std::vector<int> x_axis(SAMPLE_COUNT);
+        std::vector<int> y_axis(SAMPLE_COUNT);
+        std::vector<int> z_axis(SAMPLE_COUNT);
+
+        ptrdiff_t step_size = iio_buffer_step(buf);
+
+        for (uint16_t *sample = static_cast<uint16_t *>(iio_buffer_start(buf)); sample < iio_buffer_end(buf); sample += step_size)
+        {
+                current_samples current_sample;
+                current_sample.xpos = *sample;
+                current_sample.xneg = *(sample + sizeof(uint16_t));
+
+                current_sample.ypos = *(sample + 2 * sizeof(uint16_t));
+                current_sample.yneg = *(sample + 3 * sizeof(uint16_t));
+
+                current_sample.zpos = *(sample + 4 * sizeof(uint16_t));
+                current_sample.zneg = *(sample + 5 * sizeof(uint16_t));
+
+                cout << current_sample.xpos << ' ' << current_sample.xneg << ' '
+                     << current_sample.ypos << ' ' << current_sample.yneg << ' '
+                     << current_sample.zpos << ' ' << current_sample.zneg << endl;
+
+                x_axis.push_back(current_sample.xpos - current_sample.xneg);
+                y_axis.push_back(current_sample.ypos - current_sample.yneg);
+                z_axis.push_back(current_sample.zpos - current_sample.zneg);
+        }
+
+        // Analyze the data for significant changes, such as shocks
+        for (int i = 1; i < x_axis.size(); ++i) {
+                int x_diff = x_axis[i] - x_axis[i - 1];
+                if (x_diff > THRESHOLD) {
+                        cout << "X-axis significant change detected: " << x_diff << endl;
+                }
+
+                int y_diff = y_axis[i] - y_axis[i - 1];
+                if (y_diff > THRESHOLD) {
+                        cout << "Y-axis significant change detected: " << y_diff << endl;
+                }
+
+                int z_diff = z_axis[i] - z_axis[i - 1];
+                if (z_diff > THRESHOLD) {
+                        cout << "Z-axis significant change detected: " << z_diff << endl;
+                }
+        }
+
+        // Exit:
+        iio_context_destroy(ctx);
+        return 0;
+}
