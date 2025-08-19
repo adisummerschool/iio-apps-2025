705918ee9606bae2ede9b8a5bdbdf8fd394fa9ea
Nicoleta Bondric <nicoletabondric@gmail.com>
2025-08-19 16:33:30 +0300

    softwareApp: buffer: plotting data from the channels
    Signed-off-by: Nicoleta Bondric <nicoletabondric@gmail.com>

--- main.cpp
+++ main.cpp
@@ -5,16 +5,20 @@
 #include <unistd.h>
 #include <vector>
 #include <memory>
+
+#include "matplotlibcpp.h"
+
 #define URI "ip:10.76.84.217"
 #define DEV_NAME "ad5592r_s"
 #define SAMPLE_COUNT 32
 #define THRESHOLD 100
 #define SAMPLE_FREQUENCY 50
+namespace plt= matplotlibcpp;
 
 typedef struct {
 uint16_t xpos;
 uint16_t xneg;
-uint16_t ypos;
+uint16_t ypos;  
 uint16_t yneg;
 uint16_t zpos;
 uint16_t zneg;
@@ -49,34 +53,30 @@ return -1;
 double val=0;
 int write_trig ;
 int read_trig = iio_device_attr_read_double(trigger,"sampling_frequency", &val);
-        if(read_trig<0){
-                std::cerr<<"Read error"<<std::endl;
-                return -1;
-        }
-        else {
-                if (val != SAMPLE_FREQUENCY )
-                        write_trig = iio_device_attr_write_double(trigger,"sampling_frequency", SAMPLE_FREQUENCY);
-                         
-                if(write_trig<0){
-                std::cerr<<"write error"<<std::endl;
-                return -1;
-        }
+if(read_trig<0){
+        std::cerr<<"Read error"<<std::endl;
+        return -1;
+}
+else {
+        if (val != SAMPLE_FREQUENCY )
+                write_trig = iio_device_attr_write_double(trigger,"sampling_frequency", SAMPLE_FREQUENCY);
+                        
+        if(write_trig<0){
+        std::cerr<<"write error"<<std::endl;
+        return -1;
+}
 
-        }
-
-        
-      
-        
+}
 
 
 for(int i=0; i<6; i++)
 {      
 channels[i] = iio_device_find_channel(dev, channels_name[i], false);
-        if(!channels[i]){
+if(!channels[i]){
 std::cout<<"Channel "<<i<< " error"<<std::endl;
 return -1;
 }
-        iio_channel_enable(channels[i]);
+iio_channel_enable(channels[i]);
 }
 std::cout<<"Channels good"<<std::endl;
 
@@ -91,11 +91,15 @@ if(byte_read <0){
 std :: cerr<<"could not refill,buffer,errno: "<<errno<< std::endl;
 }
 
-std::vector <int> x_axis(SAMPLE_COUNT),y_axis(SAMPLE_COUNT),z_axis(SAMPLE_COUNT);
+int count =0;
+std::vector <int> x_axis,y_axis,z_axis,t_axis;
+
 ptrdiff_t step_size = iio_buffer_step(buf);  
 for(uint16_t *sample = static_cast<uint16_t *>(iio_buffer_start(buf));
-                sample < iio_buffer_end(buf); sample +=step_size){
-current_samples current_sample; 
+        sample < iio_buffer_end(buf); sample +=step_size){
+         
+                
+ current_samples current_sample; 
 
 memcpy(&current_sample,sample,sizeof(current_samples));
 
@@ -107,28 +111,41 @@ memcpy(&current_sample,sample,sizeof(current_samples));
 // current_sample.zneg= *(sample + 5 );
 
 
-std::cout <<"+X:"<<current_sample.xpos << " " <<"-X:"<<current_sample.xneg<< " "
-        <<"+Y:"<<current_sample.ypos << " " <<"-Y:"<<current_sample.yneg<< " "
-        <<"+Z:"<<current_sample.zpos << " " <<"-Z:"<<current_sample.zneg<< "\n";
-        
-        x_axis.push_back(current_sample.xpos-current_sample.xneg);
-        y_axis.push_back(current_sample.ypos-current_sample.yneg);
-        z_axis.push_back(current_sample.zpos-current_sample.zneg);
+// std::cout <<"+X:"<<current_sample.xpos << " " <<"-X:"<<current_sample.xneg<< " "
+// <<"+Y:"<<current_sample.ypos << " " <<"-Y:"<<current_sample.yneg<< " "
+// <<"+Z:"<<current_sample.zpos << " " <<"-Z:"<<current_sample.zneg<< "\n";
+
+x_axis.push_back(current_sample.xpos-current_sample.xneg);
+y_axis.push_back(current_sample.ypos-current_sample.yneg);
+z_axis.push_back(current_sample.zpos-current_sample.zneg);
+t_axis.push_back(count++);
 }
-        for(size_t i=1;i< x_axis.size();++i){
-                int x_diff = abs(x_axis[i]- x_axis[i - 1]);
-                if(x_diff>= THRESHOLD)
-                std:: cout <<"X axis exceeded threshold\n";
-
-                int y_diff = abs(y_axis[i]- y_axis[i - 1]);
-                if(y_diff>= THRESHOLD)
-                std:: cout <<"Y axis exceeded threshold\n";
-
-                int z_diff = abs(z_axis[i]- z_axis[i - 1]);
-                if(z_diff>= THRESHOLD)
-                std:: cout <<"Z axis exceeded threshold\n";
-        }
 
 
+// for(size_t i=1;i< x_axis.size();++i){
+//         int x_diff = abs(x_axis[i]- x_axis[i - 1]);
+//         if(x_diff>= THRESHOLD)
+//         std:: cout <<"X axis exceeded threshold\n";
+
+//         int y_diff = abs(y_axis[i]- y_axis[i - 1]);
+//         if(y_diff>= THRESHOLD)
+//         std:: cout <<"Y axis exceeded threshold\n";
+
+//         int z_diff = abs(z_axis[i]- z_axis[i - 1]);
+//         if(z_diff>= THRESHOLD)
+//         std:: cout <<"Z axis exceeded threshold\n";
+// }
+
+
+plt::figure_size(1000,600);
+plt::named_plot("X",t_axis,x_axis,"b-");
+plt::named_plot("Y",t_axis,y_axis,"r-");
+plt::named_plot("Z",t_axis,z_axis,"g-");
+plt::xlabel("Samples");
+plt::ylabel("Acc");
+plt::legend();
+plt::show();
+iio_buffer_destroy(buf);
+iio_context_destroy(ctx);
 return 0;
 }
