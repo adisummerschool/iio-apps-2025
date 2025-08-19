318d286e9d06c8242d03e0edb6935a4070deb086
Sintoma Tudor <tudor.sintoma@gmail.com>
2025-08-19 16:28:35 +0300

    Added Matplotlib (that's actally crazy)
    Signed-off-by: Sintoma Tudor <tudor.sintoma@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "matplotlibcpp.h"
 #include <iio.h>
 #include <errno.h>
 #include <string.h>
@@ -8,6 +9,8 @@
 #define DEV_NAME "ad5592r_s"
 #define SAMPLE_COUNT 32
 #define THRESHOLD 100
+
+namespace plt = matplotlibcpp;
 
 typedef struct {
 	uint16_t xpos;
@@ -29,8 +32,6 @@ int main (int argc, char **argv)
         	printf("Context error %s\n", strerror(errno));
         	return -errno;
     	}
-
-
 
 	struct iio_device *trig = iio_context_find_device(ctx, "trigger0");
 	if (!trig) {
@@ -78,6 +79,9 @@ int main (int argc, char **argv)
 	std::vector<uint16_t> x_axis;
 	std::vector<uint16_t> y_axis;
 	std::vector<uint16_t> z_axis;
+	std::vector<int> t_axis;
+
+	int count = 0;
 	for(uint16_t *sample = static_cast<uint16_t *>(iio_buffer_start(buf));
 		 sample < iio_buffer_end(buf); sample += iio_buffer_step(buf))
 	{
@@ -99,6 +103,7 @@ int main (int argc, char **argv)
 		x_axis.push_back(curr_s.xpos - curr_s.xneg);
 		y_axis.push_back(curr_s.ypos - curr_s.yneg);
 		z_axis.push_back(curr_s.zpos - curr_s.zneg);
+		t_axis.push_back(count++);
 	}
 
 	for(size_t i = 1; i < x_axis.size(); ++i)
@@ -127,6 +132,16 @@ int main (int argc, char **argv)
                         std::cout << "Z axis exceeded threshold" << "\n";
                 }
         }
+
+	plt::figure_size(1000, 600);
+	plt::named_plot("X", t_axis, x_axis, "b-");
+	plt::named_plot("Y", t_axis, y_axis, "r-");
+	plt::named_plot("Z", t_axis, z_axis, "g-");
+	plt::xlabel("Samples");
+	plt::ylabel("Acc");
+	plt::legend();
+	plt::show();
+
 	iio_context_destroy(ctx);
 	return 0;
 }
