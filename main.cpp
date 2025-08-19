76d44165cd0a7b090eed2825a146d6b59685b979
Darius Mester <mesterdariusdragos000@gmail.com>
2025-08-19 16:18:28 +0300

    added an plot graph and linked the python library
    Signed-off-by: Darius Mester <mesterdariusdragos000@gmail.com>

--- main.cpp
+++ main.cpp
@@ -4,12 +4,15 @@
 #include <errno.h>
 #include <string.h>
 #include <vector>
+#include "matplotlibcpp.h"
 
 #define URI "ip:10.76.84.137"
 #define DEV_NAME "iio-adc-ad5592"
 #define SAMPLE_COUNT 32
 #define THR 100
 #define SAMPLING_FREQ 50
+
+namespace plt = matplotlibcpp;
 
 typedef struct {
         uint16_t xpos, xneg, ypos, yneg, zpos, zneg;
@@ -72,9 +75,9 @@ int main(int argc, char** argv){
         if (bytes_read < 0){
                 std::cerr<<"Could not refill buffer, errno: "<<errno<<'\n';
         }
-
+        int i=0;
         ptrdiff_t step_size = iio_buffer_step(buf);
-        std::vector<int32_t> x_axis(SAMPLE_COUNT), y_axis(SAMPLE_COUNT), z_axis(SAMPLE_COUNT);
+        std::vector<int32_t> x_axis, y_axis, z_axis, t_axis;
         for (uint16_t *sample = static_cast<uint16_t *>(iio_buffer_start(buf)); sample < iio_buffer_end(buf); sample += step_size){
                 current_samples current_sample;
                 memcpy(&current_sample, sample, sizeof(current_sample));
@@ -82,22 +85,31 @@ int main(int argc, char** argv){
                 x_axis.push_back(current_sample.xpos - current_sample.xneg);
                 y_axis.push_back(current_sample.ypos - current_sample.yneg);
                 z_axis.push_back(current_sample.zpos - current_sample.zneg);
+                t_axis.push_back(i++);
         }
 
-        for (size_t i = 1; i<x_axis.size(); ++i){
-                int32_t x_diff = abs(x_axis[i] - x_axis[i-1]);
-                int32_t y_diff = abs(y_axis[i] - y_axis[i-1]);
-                int32_t z_diff = abs(z_axis[i] - z_axis[i-1]);
-                if (x_diff > THR){
-                        printf("X Threshold Exceeded");
-                }
-                if (y_diff > THR){
-                        printf("Y Threshold Exceeded");
-                }
-                if (z_diff > THR){
-                        printf("Z Threshold Exceeded");
-                }
-        }
+        // for (size_t i = 1; i<x_axis.size(); ++i){
+        //         int32_t x_diff = abs(x_axis[i] - x_axis[i-1]);
+        //         int32_t y_diff = abs(y_axis[i] - y_axis[i-1]);
+        //         int32_t z_diff = abs(z_axis[i] - z_axis[i-1]);
+        //         if (x_diff > THR){
+        //                 printf("X Threshold Exceeded");
+        //         }
+        //         if (y_diff > THR){
+        //                 printf("Y Threshold Exceeded");
+        //         }
+        //         if (z_diff > THR){
+        //                 printf("Z Threshold Exceeded");
+        //         }
+        // }
 
+        plt::figure_size(1000,600);
+        plt::named_plot("X", t_axis, x_axis, "b-");
+        plt::named_plot("Y", t_axis, y_axis, "r-");
+        plt::named_plot("Z", t_axis, z_axis, "g-");
+        plt::xlabel("Samples");
+        plt::ylabel("Acc");
+        plt::legend();
+        plt::show();
         return 0;
 }
