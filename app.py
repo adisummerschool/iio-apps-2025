a1ad7a5df01cd56d860cf8cdd6127967b0a13d7a
Evelyn Plesca <evelyn-iulia.plesca@analog.com>
2025-08-21 12:13:22 +0300

    front_end: Add key simulator application that takes accelerometer measurements and displays them visually in a WASD interface
    Signed-off-by: Evelyn Plesca <evelyn-iulia.plesca@analog.com>

---
+++ app.py
@@ -0,0 +1,196 @@
+from logging import root
+import math
+import time
+from time import sleep
+from queue import Queue
+from threading import Thread, Event
+
+import iio
+import tkinter as tk
+from pynput import keyboard
+from pynput.keyboard import Controller
+
+URI = "ip:10.76.84.219"
+DEVICE = "iio-ad5592r-s"
+TIMEOUT = 3
+
+def init_device():
+        ctx = iio.Context(URI)
+        device = ctx.find_device(DEVICE)
+        
+        if device is None:
+                raise ValueError("No device")
+        
+        return device
+
+def get_data(device: iio):
+        channel_names = [f'voltage{i}' for i in range(6)]
+        axes = ['x', 'y', 'z']
+        axis_data = {
+                'x': {'+': int, '-': int},
+                'y': {'+': int, '-': int},
+                'z': {'+': int, '-': int}
+        }
+        
+        for i, channel_name in enumerate(channel_names):
+                channel = device.find_channel(channel_name)
+                if channel is None:
+                        raise ValueError(f"Channel {channel_name} not found")
+                
+                raw_value = int(channel.attrs['raw'].value)
+                axis = axes[i // 2]
+                sign = '-' if i % 2 == 0 else '+'
+                axis_data[axis][sign] = raw_value
+
+        print("Axis data:", axis_data)
+
+        return axis_data
+
+def get_roll_pitch(data):
+        x_accel = data['x']['+'] - data['x']['-']
+        y_accel = data['y']['+'] - data['y']['-']
+        z_accel = data['z']['+'] - data['z']['-']
+
+        roll = math.atan2(-x_accel, math.sqrt(y_accel**2 + z_accel**2)) * 180 / math.pi
+        pitch = math.atan2(y_accel, z_accel) * 180 / math.pi
+
+        return roll, pitch
+
+def get_movement(roll, pitch):
+        movement = {'left': 0, 'right': 0, 'front': 0, 'back': 0}
+                
+        limit = 45
+                
+        if roll < 0:
+                movement['front'] = min(1, abs(roll) / limit)
+        else:
+                movement['back'] = min(1, abs(roll) / limit)
+
+        if pitch < 0:
+                movement['left'] = min(1, abs(pitch) / limit)
+        else:
+                movement['right'] = min(1, abs(pitch) / limit)
+
+        print("Movement:", movement)
+
+        return movement
+
+def simulate_key(key, on_time):
+        kb_controller = Controller()
+        
+        if on_time > 0:
+                kb_controller.press(key)
+                sleep(on_time)
+                kb_controller.release(key)
+
+def start_iio(device: iio.Device):
+        start = time.time()
+        
+        data = get_data(device)
+        roll, pitch = get_roll_pitch(data)
+        movement = get_movement(roll, pitch)
+
+        time_elapsed = time.time() - start
+
+        for direction, key in [('front', 'w'), ('left', 'a'), ('right', 'd'), ('back', 's')]:
+                value = movement[direction]
+                
+                if value > 0.2:
+                        on_time = TIMEOUT * value
+                        key_thread = Thread(target=simulate_key, args=(key, on_time))
+                        key_thread.daemon = True
+                        key_thread.start()
+                        
+        sleep(TIMEOUT - time_elapsed)
+        
+        return movement
+
+def create_gui(queue):
+        def _from_rgb(rgb):
+                return "#%02x%02x%02x" % rgb        
+        
+        def update_sq_color(canvas, sq, value):
+                value = 1 - value
+                canvas.itemconfig(sq, fill = _from_rgb( (255, int(255*value), int(255*value)) ) )  
+        
+        def update_gui():
+                movement = queue.get()
+                update_sq_color(canvas, squares['left'],  movement['left'])
+                update_sq_color(canvas, squares['right'], movement['right'])
+                update_sq_color(canvas, squares['front'], movement['front'])
+                update_sq_color(canvas, squares['back'],  movement['back'])
+                # print(movement)
+                root.after(int(TIMEOUT * 1000), update_gui)
+
+        root = tk.Tk()
+        root.title('key simulator')
+
+        canvas = tk.Canvas(root, width=400, height=400)
+        canvas.pack()
+
+        square_size = 100
+        squares = {
+                'left'  : canvas.create_rectangle(50, 150, 50 + square_size, 150 + square_size, fill= 'white'),
+                'right' : canvas.create_rectangle(250, 150, 250 + square_size, 150 + square_size, fill= 'white'),
+                'front' : canvas.create_rectangle(150, 50, 150 + square_size, 50 + square_size, fill= 'white'),
+                'back'  : canvas.create_rectangle(150, 250, 150 + square_size, 250 + square_size, fill= 'white'),
+        }
+        update_gui()
+
+        root.mainloop()
+
+def simulate_movement(queue):
+        device = init_device()
+        
+        while not exit_event.is_set():
+                if start_event.is_set():
+                        queue.put(start_iio(device))
+                        movement = start_iio(device)
+                        print("Simulating movement...")
+                        
+                        # if movement is not None:
+                        #     queue.put(movement)
+                        #     mvm = queue.get()
+                        #     mvm['left'] += 1
+                        #     queue.put(mvm)
+                        
+                        # sleep(1)
+                start_event.wait()
+def on_keypress(key):
+    if key == keyboard.Key.ctrl_l:
+        if (start_event.is_set()):
+            start_event.clear()
+            print("Pause")
+            return True
+        else:
+            start_event.set()
+            print("Start")
+            return True
+    elif key == keyboard.Key.esc:
+        exit_event.set()
+        print("Exit")
+        return False
+
+if __name__ == "__main__":
+        start_event = Event()
+        exit_event = Event()
+        
+        init_movements = {'left': 0, 'right': 0, 'front': 0, 'back': 0 }
+        movement_q = Queue()
+        movement_q.put(init_movements)
+        
+        gui_thread = Thread(target=create_gui, args=(movement_q,))
+        gui_thread.daemon = True
+        gui_thread.start()
+
+        iio_thread = Thread(target=simulate_movement, args=(movement_q,))
+        iio_thread.daemon = True
+        iio_thread.start()
+        
+        print("Press Left CTRL so start/stop application. Press ESC to exit")
+
+        with keyboard.Listener(on_press=on_keypress) as listener:
+                listener.join()
+                
+                
+        # gui_thread.join()
